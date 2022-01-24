f37819bcf0b3316b5e5f18ee634f9fc458c8efec
Vikram <vikram@testsigma.com>
2022-01-24 12:28:20 +0530

    build changes

--- deploy/docker/build.sh
+++ deploy/docker/build.sh
@@ -3,8 +3,9 @@
 set -x
 set -e
 
-VERSION=$1
-EXPECTED_ARGS=1
+DOCKER_VERSION=$1
+AGENT_TAG=$2
+EXPECTED_ARGS=2
 E_BADARGS=65
 CURRENT_DIR="$(pwd -P)"
 
@@ -22,11 +23,11 @@ ROOT_FOLDER="$(
 
 cd "$ROOT_FOLDER" || exit 1
 
-sh $ROOT_FOLDER/deploy/compile.sh --UI_BUILD_CONF=docker --LOCAL_AGENT_TAG=$VERSION
+sh $ROOT_FOLDER/deploy/compile.sh --UI_BUILD_CONF=docker --LOCAL_AGENT_TAG=$AGENT_TAG
 
 docker build -t server -f $ROOT_FOLDER/Dockerfile .
 
-docker tag server:latest testsigmahq/server:$VERSION
-docker push testsigmahq/server:$VERSION
+docker tag server:latest testsigmahq/server:$DOCKER_VERSION
+docker push testsigmahq/server:$DOCKER_VERSION
 
 cd $CURRENT_DIR

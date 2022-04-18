013f15f25fe777e68cd9ddb11a06444259a7e490
Shanmukha sharma <shanmukha@testsigma.com>
2022-04-18 10:23:12 +0530

    Migration entity_type change

--- server/src/main/resources/db/migration/V19__test_step_screenshot_change.sql
+++ server/src/main/resources/db/migration/V19__test_step_screenshot_change.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `test_step_screenshots` ADD COLUMN `entity_type` VARCHAR(30) DEFAULT NULL;
-UPDATE test_step_screenshots SET entity_type = 'EXECUTION';
+UPDATE test_step_screenshots SET entity_type = 'TEST_PLAN';

bc6a1e8e97a12349a6272410120f097a71dfcfdb
ManikantaReddy <manikanta.reddy@testsigma.com>
2022-09-12 17:47:00 +0530

    TOS-661 warning message alignment

--- server/src/main/java/com/testsigma/specification/DryTestPlanSpecification.java
+++ server/src/main/java/com/testsigma/specification/DryTestPlanSpecification.java
@@ -1,9 +1,44 @@
 package com.testsigma.specification;
 
 import com.testsigma.model.DryTestPlan;
+import lombok.extern.log4j.Log4j2;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+@Log4j2
 public class DryTestPlanSpecification extends BaseSpecification<DryTestPlan> {
   public DryTestPlanSpecification(SearchCriteria searchCriteria) {
     super(searchCriteria);
   }
+
+  @Override
+  protected Object getEnumValueIfEnum(String key, Object value, SearchOperation operation) {
+    switch (key){
+      case "createdDate":
+        return parseDate(value, operation);
+      case "updatedDate":
+        return parseDate(value, operation);
+      default:
+        return super.getEnumValueIfEnum(key, value, operation);
+    }
+  }
+
+  private Object parseDate(Object value, SearchOperation op) {
+    String valueStr = value.toString();
+    if (op.equals(SearchOperation.LESS_THAN))
+      valueStr = valueStr + " 23:59:59";
+    if (op.equals(SearchOperation.GREATER_THAN))
+      valueStr = valueStr + " 00:00:00";
+    DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
+    try {
+      return format.parse(valueStr);
+    } catch (ParseException e) {
+      log.error(e.getMessage(), e);
+      return null;
+    }
+  }
+
 }
--- server/src/main/java/com/testsigma/specification/TestDataProfileSpecification.java
+++ server/src/main/java/com/testsigma/specification/TestDataProfileSpecification.java
@@ -2,9 +2,15 @@ package com.testsigma.specification;
 
 import com.testsigma.model.TestCase;
 import com.testsigma.model.TestData;
+import lombok.extern.log4j.Log4j2;
 
 import javax.persistence.criteria.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
+@Log4j2
 public class TestDataProfileSpecification extends BaseSpecification<TestData> {
   public TestDataProfileSpecification(final SearchCriteria criteria) {
     super(criteria);
@@ -23,7 +29,14 @@ public class TestDataProfileSpecification extends BaseSpecification<TestData> {
 
   @Override
   protected Object getEnumValueIfEnum(String key, Object value, SearchOperation operation) {
-    return super.getEnumValueIfEnum(key, value, operation);
+    switch (key){
+      case "createdDate":
+        return parseDate(value, operation);
+      case "updatedDate":
+        return parseDate(value, operation);
+      default:
+        return super.getEnumValueIfEnum(key, value, operation);
+    }
   }
 
   @Override
@@ -43,6 +56,20 @@ public class TestDataProfileSpecification extends BaseSpecification<TestData> {
       return predicate;
     } else {
       return super.toPredicate(root, query, builder);
+    }
+  }
+  private Object parseDate(Object value, SearchOperation op) {
+    String valueStr = value.toString();
+    if (op.equals(SearchOperation.LESS_THAN))
+      valueStr = valueStr + " 23:59:59";
+    if (op.equals(SearchOperation.GREATER_THAN))
+      valueStr = valueStr + " 00:00:00";
+    DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
+    try {
+      return format.parse(valueStr);
+    } catch (ParseException e) {
+      log.error(e.getMessage(), e);
+      return null;
     }
   }
 }

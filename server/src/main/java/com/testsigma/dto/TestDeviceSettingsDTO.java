7c4ad0aa36a76b392e616ffda666fbb8177d58bf
ManikantaReddy <manikanta.reddy@testsigma.com>
2022-09-22 13:57:29 +0530

    Removed Unused Import

--- server/src/main/java/com/testsigma/dto/TestDeviceSettingsDTO.java
+++ server/src/main/java/com/testsigma/dto/TestDeviceSettingsDTO.java
@@ -3,7 +3,6 @@ package com.testsigma.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.testsigma.model.AppPathType;
-import com.testsigma.model.TestPlanLabType;
 import lombok.Data;
 
 @Data

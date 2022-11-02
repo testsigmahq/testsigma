2d67d878d10c266bdd75810faaa1f44b786d85ba
Tarunava Dutta <tarunava.dutta@testsigma.com>
2022-11-02 11:44:16 +0530

    Fix for test data set api

--- server/src/main/java/com/testsigma/controller/recorder/TestDataSetsRecorderController.java
+++ server/src/main/java/com/testsigma/controller/recorder/TestDataSetsRecorderController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/os_recorder/test_data_sets", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@RequestMapping(value = "/os_recorder/test_data_sets")
 @Log4j2
 @RequiredArgsConstructor(onConstructor = @__({@Autowired}))
 public class TestDataSetsRecorderController {

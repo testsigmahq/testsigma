90efeda9a06ca04ff12e84b300f36218c52686b0
Tarunava Dutta <tarunava.dutta@testsigma.com>
2022-11-03 16:14:08 +0530

    Removed unnecessary code

--- server/src/main/java/com/testsigma/controller/recorder/DefaultDataRecorderGeneratorController.java
+++ server/src/main/java/com/testsigma/controller/recorder/DefaultDataRecorderGeneratorController.java
@@ -2,9 +2,7 @@ package com.testsigma.controller.recorder;
 
 import com.testsigma.dto.DefaultDataGeneratorsDTO;
 import com.testsigma.mapper.DefaultDataGeneratorMapper;
-import com.testsigma.mapper.recorder.DefaultDataGeneratorRecorderMapper;
 import com.testsigma.model.DefaultDataGenerator;
-import com.testsigma.model.recorder.DefaultDataGeneartorRecorderFunctionDTO;
 import com.testsigma.service.DefaultDataGeneratorService;
 import com.testsigma.specification.DefaultDataGeneratorSpecificationsBuilder;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,20 +26,16 @@ public class DefaultDataRecorderGeneratorController {
 
     private final DefaultDataGeneratorMapper testDataFunctionMapper;
     private final DefaultDataGeneratorService defaultDataGeneratorService;
-    private final DefaultDataGeneratorRecorderMapper defaultDataGeneratorRecorderMapper;
 
     @GetMapping
-    public Page<DefaultDataGeneartorRecorderFunctionDTO> index(DefaultDataGeneratorSpecificationsBuilder builder, Pageable pageable) {
-        //Specification<DefaultDataGenerator> specification = builder.build();
+    public Page<DefaultDataGeneratorsDTO> index(DefaultDataGeneratorSpecificationsBuilder builder, Pageable pageable) {
         Page<DefaultDataGenerator> testDataFunctions = defaultDataGeneratorService.findAll(pageable);
         List<DefaultDataGeneratorsDTO> dtos = testDataFunctionMapper.mapToDTO(testDataFunctions.getContent());
-        List<DefaultDataGeneartorRecorderFunctionDTO> results = defaultDataGeneratorRecorderMapper.mapDTOs(dtos);
-        return new PageImpl<>(results, pageable, testDataFunctions.getTotalElements());
+        return new PageImpl<>(dtos, pageable, testDataFunctions.getTotalElements());
     }
 
     @GetMapping(path = "/{id}")
-    public DefaultDataGeneartorRecorderFunctionDTO show(@PathVariable(value = "id") Long id) throws Exception {
-        DefaultDataGeneratorsDTO defaultDataGeneratorsDTO = testDataFunctionMapper.mapToDTO(defaultDataGeneratorService.find(id));
-        return defaultDataGeneratorRecorderMapper.mapDTO(defaultDataGeneratorsDTO);
+    public DefaultDataGeneratorsDTO show(@PathVariable(value = "id") Long id) throws Exception {
+        return testDataFunctionMapper.mapToDTO(defaultDataGeneratorService.find(id));
     }
 }

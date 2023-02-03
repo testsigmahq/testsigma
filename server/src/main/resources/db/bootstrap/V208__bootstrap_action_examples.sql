2a9b5d133b8be2b2a10ccc7214f61105526861e3
Tarunava Dutta <tarunava.dutta@testsigma.com>
2023-02-03 12:43:28 +0530

    (fix): Update nlps and examples

--- server/src/main/resources/db/bootstrap/V208__bootstrap_action_examples.sql
+++ server/src/main/resources/db/bootstrap/V208__bootstrap_action_examples.sql
@@ -547,6 +547,11 @@ INSERT INTO `natural_text_action_examples` (`id`, `natural_text_action_id`, `des
 INSERT INTO `natural_text_action_examples` (`id`, `natural_text_action_id`, `description`, `example`, `workspace`, `data`, `created_date`, `updated_date`) VALUES (850,20142,'Break loop','Breaks Loop','https://app.testsigma.com/','{}','2022-01-10 20:16:55','2022-01-10 20:16:55');
 INSERT INTO `natural_text_action_examples` (`id`, `natural_text_action_id`, `description`, `example`, `workspace`, `data`, `created_date`, `updated_date`) VALUES (851,20143,'Continue loop','Continues loop','https://app.testsigma.com/','{}','2022-01-10 20:16:55','2022-01-10 20:16:55');
 
+INSERT INTO `natural_text_action_examples` (`id`, `natural_text_action_id`, `description`, `example`, `workspace`, `data`, `created_date`, `updated_date`) VALUES (852, 1106,'Store test-data1 in test-data2','In this example, we can store any data into a runtime variable.', '{}','{}','2023-01-28 16:06:57','2023-01-28 16:06:57');
+INSERT INTO `natural_text_action_examples` (`id`, `natural_text_action_id`, `description`, `example`, `workspace`, `data`, `created_date`, `updated_date`) VALUES (853, 10222,'Store test-data1 in test-data2','In this example, we can store any data into a runtime variable.', '{}','{}','2023-01-28 16:06:57','2023-01-28 16:06:57');
+INSERT INTO `natural_text_action_examples` (`id`, `natural_text_action_id`, `description`, `example`, `workspace`, `data`, `created_date`, `updated_date`) VALUES (854, 20172,'Store test-data1 in test-data2','In this example, we can store any data into a runtime variable.', '{}','{}','2023-01-28 16:06:57','2023-01-28 16:06:57');
+INSERT INTO `natural_text_action_examples` (`id`, `natural_text_action_id`, `description`, `example`, `workspace`, `data`, `created_date`, `updated_date`) VALUES (855, 30172,'Store test-data1 in test-data2','In this example, we can store any data into a runtime variable.', '{}','{}','2023-01-28 16:06:57','2023-01-28 16:06:57');
+
 /*!40000 ALTER TABLE `natural_text_action_examples` ENABLE KEYS */;
 UNLOCK TABLES;
 

0ab2aa2f97bb9b210078c9671ae854b5c7ca07ed
Tarunava Dutta <tarunava.dutta@testsigma.com>
2023-02-06 13:36:12 +0530

    [TOS-967] (fix): Workspace version number invalid format

--- server/src/main/resources/db/bootstrap/V202__bootstrap_application_versions.sql
+++ server/src/main/resources/db/bootstrap/V202__bootstrap_application_versions.sql
@@ -7,7 +7,7 @@ LOCK TABLES `workspace_versions` WRITE;
 INSERT INTO `workspace_versions` (`id`, `workspace_id`, `description`, `version_name`, `copied_from`, `created_date`, `updated_date`) VALUES (34,21,NULL,'Version1.0',NULL,'2019-02-19 06:55:54',CURRENT_TIMESTAMP);
 INSERT INTO `workspace_versions` (`id`, `workspace_id`, `description`, `version_name`, `copied_from`, `created_date`, `updated_date`) VALUES (35,22,NULL,'Version1.0',NULL,'2019-02-19 07:05:22',CURRENT_TIMESTAMP);
 INSERT INTO `workspace_versions` (`id`, `workspace_id`, `description`, `version_name`, `copied_from`, `created_date`, `updated_date`) VALUES (36,23,NULL,'Version1.0',NULL,'2019-02-19 07:06:01',CURRENT_TIMESTAMP);
-INSERT INTO `workspace_versions` (`id`, `workspace_id`, `description`, `version_name`, `copied_from`, `created_date`, `updated_date`) VALUES (37,24,NULL,'Version 01',NULL,'2019-02-19 08:34:25',CURRENT_TIMESTAMP);
+INSERT INTO `workspace_versions` (`id`, `workspace_id`, `description`, `version_name`, `copied_from`, `created_date`, `updated_date`) VALUES (37,24,NULL,'Version1.0',NULL,'2019-02-19 08:34:25',CURRENT_TIMESTAMP);
 INSERT INTO `workspace_versions` (`id`, `workspace_id`, `description`, `version_name`, `copied_from`, `created_date`, `updated_date`) VALUES (39,25,NULL,'Default Version',NULL,'2019-09-06 06:38:44',CURRENT_TIMESTAMP);
 INSERT INTO `workspace_versions` (`id`, `workspace_id`, `description`, `version_name`, `copied_from`, `created_date`, `updated_date`) VALUES (43,29,NULL,'Default Version',NULL,'2019-09-16 11:14:44',CURRENT_TIMESTAMP);
 INSERT INTO `workspace_versions` (`id`, `workspace_id`, `description`, `version_name`, `copied_from`, `created_date`, `updated_date`) VALUES (44,30,NULL,'Default Version',NULL,'2019-09-16 11:15:10',CURRENT_TIMESTAMP);
---
+++ server/src/main/resources/db/migration/V58__update_workspace_version.sql
@@ -0,0 +1,1 @@
+UPDATE `workspace_versions` SET `version_name`='Version1.0' WHERE id=37;

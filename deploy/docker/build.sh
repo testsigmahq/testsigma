a2f91a34ec7997a28b2baac71ce5698830343394
Tarunava Dutta <tarunava.dutta@testsigma.com>
2023-02-20 21:22:01 +0530

    Fix buildx command

--- deploy/docker/build.sh
+++ deploy/docker/build.sh
@@ -52,9 +52,9 @@ cd "$ROOT_FOLDER" || exit 1
 sh $ROOT_FOLDER/deploy/compile.sh --UI_BUILD_CONF=docker --LOCAL_AGENT_TAG=$AGENT_TAG
 
 echo "Starting build..."
-docker buildx build --platform linux/amd64,linux/arm64 -t server -f $ROOT_FOLDER/Dockerfile .
+docker buildx build --platform linux/amd64,linux/arm64 -t testsigmahq/$IMAGE_NAME:$DOCKER_VERSION -f $ROOT_FOLDER/Dockerfile --push .
 
-docker tag server:latest testsigmahq/$IMAGE_NAME:$DOCKER_VERSION
-docker push testsigmahq/$IMAGE_NAME:$DOCKER_VERSION
+#docker tag server:latest testsigmahq/$IMAGE_NAME:$DOCKER_VERSION
+#docker push testsigmahq/$IMAGE_NAME:$DOCKER_VERSION
 
 cd $CURRENT_DIR
